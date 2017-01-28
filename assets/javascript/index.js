d75e24b0567cc4d843b520566e89e12462825593
chrisxjacobi <chrisxjacobi@gmail.com>
2017-01-28 00:14:51 -0600

    styling changes for navbar

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -15,7 +15,7 @@
 
 $(document).ready(function() {
   $('.button-collapse').sideNav({
-      menuWidth: 240, // Default is 240
+      menuWidth: 180, // Default is 240
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
