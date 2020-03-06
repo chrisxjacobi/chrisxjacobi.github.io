97f5ff0b8d1e41253a8d5da201b87edc121e23b4
chrisxjacobi <chrisxjacobi@gmail.com>
2020-03-06 16:48:00 -0600

    attempt at sending form data

--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
 $(document).ready(function () {
 
 
-//    $("#submit-button").click(function (e) {
-//        e.preventDefault();
-//        $("#greeting-box").html("thanks!");
-//        //sendForm();
-//    })
+    $("#submit-button").click(function (e) {
+        e.preventDefault();
+        $("#greeting-box").html("thanks!");
+        sendForm();
+    })
 
-//    function sendForm() {
-//        var request = new XMLHttpRequest();
-//        var memberForm = document.getElementById("send-form");
-//        var formData = new FormData(memberForm);
-//        var greetingBox = document.getElementById("greeting-box");
-//
-//        request.addEventListener("load", function (e) {
-//
-//            if (e.target.responseText == "okay") {
-//                if (e.target.status == 200) {
-//                    //clear();
-//                    greetingBox.innerHTML = "Sent!";
-//                }
-//            }
-//        });
-//
-//        request.addEventListener("error", function (e) {
-//
-//            if (e.target.status >= 400) {
-//                //clear();
-//                greetingBox.innerHTML = "Error!;"
-//            }
-//        });
-//
-//        request.open('POST', 'email.php');
-//        request.send(formData);
-//    };
+    function sendForm() {
+        var request = new XMLHttpRequest();
+        var memberForm = document.getElementById("send-form");
+        var formData = new FormData(memberForm);
+        var greetingBox = document.getElementById("greeting-box");
+
+        request.addEventListener("load", function (e) {
+
+            if (e.target.responseText == "okay") {
+                if (e.target.status == 200) {
+                    //clear();
+                    greetingBox.innerHTML = "Sent!";
+                }
+            }
+        });
+
+        request.addEventListener("error", function (e) {
+
+            if (e.target.status >= 400) {
+                //clear();
+                greetingBox.innerHTML = "Error!;"
+            }
+        });
+
+        request.open('POST', 'contact.php');
+        request.send(formData);
+    };
 
 
     // animate movement on page
