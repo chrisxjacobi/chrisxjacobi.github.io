bff52afecd2bac38c6e2082d66edc2dd69075800
chrisxjacobi <chrisxjacobi@gmail.com>
2020-03-06 16:04:14 -0600

    new contact.php file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,42 @@
 $(document).ready(function () {
+
+
+    $("#submit-button").click(function (e) {
+        e.preventDefault();
+        $("#greeting-box").html("thanks!");
+        //sendForm();
+    })
+
+//    function sendForm() {
+//        var request = new XMLHttpRequest();
+//        var memberForm = document.getElementById("send-form");
+//        var formData = new FormData(memberForm);
+//        var greetingBox = document.getElementById("greeting-box");
+//
+//        request.addEventListener("load", function (e) {
+//
+//            if (e.target.responseText == "okay") {
+//                if (e.target.status == 200) {
+//                    //clear();
+//                    greetingBox.innerHTML = "Sent!";
+//                }
+//            }
+//        });
+//
+//        request.addEventListener("error", function (e) {
+//
+//            if (e.target.status >= 400) {
+//                //clear();
+//                greetingBox.innerHTML = "Error!;"
+//            }
+//        });
+//
+//        request.open('POST', 'email.php');
+//        request.send(formData);
+//    };
+
+
+    // animate movement on page
     $(function () {
         $('a[href*="#"]:not([href="#"])').click(function () {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -16,5 +54,3 @@ $(document).ready(function () {
 
 
 });
-
-//
