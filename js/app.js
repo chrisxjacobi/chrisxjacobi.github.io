8931a2fdaff2fb53246b0050f673ef2f7b94a742
chrisxjacobi <chrisxjacobi@gmail.com>
2020-03-06 16:52:09 -0600

    testing response with ajax jquery

--- js/app.js
+++ js/app.js
@@ -1,56 +1,83 @@
+/*************
+Name: Chris Jacobi
+Coding 05
+Purpose: JavaScript file reconfigured for jQuery functionality - will be easier for programmer to write and for user to read.
+*************/
+
+// ready() function necessary to run program
 $(document).ready(function () {
 
+    /
+    $("#submit-button").click(function () {
+        var error = validate();
 
-    $("#submit-button").click(function (e) {
-        e.preventDefault();
-        $("#greeting-box").html("thanks!");
-        sendForm();
-    })
-
-    function sendForm() {
-        var request = new XMLHttpRequest();
-        var memberForm = document.getElementById("send-form");
-        var formData = new FormData(memberForm);
-        var greetingBox = document.getElementById("greeting-box");
-
-        request.addEventListener("load", function (e) {
-
-            if (e.target.responseText == "okay") {
-                if (e.target.status == 200) {
-                    //clear();
-                    greetingBox.innerHTML = "Sent!";
-                }
-            }
-        });
-
-        request.addEventListener("error", function (e) {
-
-            if (e.target.status >= 400) {
-                //clear();
-                greetingBox.innerHTML = "Error!;"
-            }
-        });
-
-        request.open('POST', 'contact.php');
-        request.send(formData);
-    };
-
-
-    // animate movement on page
-    $(function () {
-        $('a[href*="#"]:not([href="#"])').click(function () {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 750);
-                    return false;
-                }
-            }
-        });
+        if (error === "") {
+            $("#greeting-box").html("Sending...")
+            sendForm();
+        } else {
+            $("#greeting-box").html(error);
+        }
     });
 
+    // Function validates that input boxes are filled out, whitespace is trimmed, and all (if any) warnings are returned as this function's value for future use
+    function validate() {
+        var name = $("#name").val().trim();
+        var email = $("#email").val().trim();
+        var message = $("#message").val().trim();
+        var warning = "";
 
+        if (name === "") {
+            warning += "Must supply a name. <br>";
+        }
+
+        if (!validEmail(email)) {
+            warning += "Email address must be in a valid format. <br>"
+        }
+
+        if (email !== emailCheck) {
+            warning += "Email addresses must match. <br>";
+        }
+
+        if (subject === "") {
+            warning += "Must supply a subject. <br>"
+        }
+
+        if (message === "") {
+            warning += "Must supply a message. <br>"
+        }
+
+        return warning;
+    };
+
+    // Function creates a URL string with returnd form data using the jQuery serialize() method. .ajax() performs an HTTP request with the URL string form data, the type of request, and the URL to which the request is sent. If successful, form data is cleared, the developer has response information (for peace of mind!) and the user is informed that the form has been sent. If there is an error, the form is cleared, the developer is informed of error specifics, and an error message is displayed to the user.
+    function sendForm() {
+        var myData = $("#member-form").serialize();
+
+        $.ajax({
+            data: myData,
+            type: 'POST',
+            url: 'contact.php',
+            success: function (responseText) {
+                console.log("response: " + responseText);
+                $("#member-form input[type='text'], input[type='email'], textarea").val("");
+                $("#warning-box").text("Sent!");
+
+                // Test to ensure correct form data is actually being sent
+                // $("#warning-box").text(myData);
+            },
+            error: function (request, status) {
+                console.log("status: " + request.status);
+                console.log("response: " + request.responseText);
+                $("#member-form input[type='text'], input[type='email'], textarea").val("");
+                $("#greeting-box").text("Error!");
+            },
+        });
+    };
+
+    // Function provided that validates email addresses to ensure they are in valid format
+    function validEmail(email) {
+        var re =
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\ -0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
 });
